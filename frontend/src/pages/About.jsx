d842eed47cfb51c14db612f9fd8bc471e4e8c3cf
WilliamAckerman <148882361+WilliamAckerman@users.noreply.github.com>
2025-11-22 17:34:42 -0500

    Made adjustments to about page and privacy policy based on backend switch from Render to Vercel.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -91,9 +91,9 @@ function About() {
                 frontend is being deployed using Vercel.
               </p>
               <p className="text-base mt-2">
-                This website's backend was created using Node.js and Express.js to create a RESTful API that communicates with a MongoDB database. {/*The contact form 
-                also makes use of Nodemailer, Google's OAuth 2.0, and the Gmail API v1 to send messages users submit through the contact form to my Gmail inbox.*/} 
-                This website's backend is being deployed using Render.
+                This website's backend was created using Node.js and Express.js to create a RESTful API that communicates with a MongoDB database. The contact form 
+                also makes use of Nodemailer and Gmail to send messages users submit through the contact form to my Gmail inbox. 
+                This website's backend is being deployed using Vercel.
               </p>
               <p className="text-base mt-2">
                 By using this website, you agree to this website's <Link 
