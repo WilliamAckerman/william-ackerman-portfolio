d89b0f16fb9d3268d57634943392693fb42bb2d4
WilliamAckerman <148882361+WilliamAckerman@users.noreply.github.com>
2026-04-08 12:52:32 -0400

    Updated Privacy Policy.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -202,7 +202,7 @@ function Home() {
               </p>
 
               <p className={`${homeBoxText}`}>
-                <strong>Effective Date:</strong> January 1, 2026
+                <strong>Effective Date:</strong> April 8, 2026
               </p>
             </div>
           </section>
